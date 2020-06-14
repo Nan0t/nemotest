8bcc393e6bfe7fe1856821665551945f0c723a43
Hernan <hsolis@est.frba.utn.edu.ar>
2020-06-14 01:16:17 -0300

    [feat] se matchean cartas y se van borrando

--- index.js
+++ index.js
@@ -3,15 +3,76 @@
 // Posiciones de cartas random en cada juego
 // tener un timer
 // mostrar intentos
-let colores=["red","blue","darkgreen","cyan","darkslateblue","indigo","saddlebrown","aliceblue"];
+let colores=["red","blue","darkgreen","cyan","darkslateblue","indigo","saddlebrown","aliceblue","red","blue","darkgreen","cyan","darkslateblue","indigo","saddlebrown","aliceblue"];
 let cartasDisponibles= document.querySelectorAll(".card");
+let cartasAsignadas=[];
+let cartasClickeadas=[];
+let intentos=0;
 let botonInicio= document.getElementById("botonJugar");
-botonInicio.onclick= IniciarJuego;
+botonInicio.onclick= iniciarJuego;
 
-function IniciarJuego(){
+function iniciarJuego(){
+    let cartasDisponibles= document.querySelectorAll(".card");
     asignarPosicionesAleatoriamente();
+    manejarRonda();
 };
 
 function asignarPosicionesAleatoriamente(){
-
+    cartasDisponibles.forEach(function(nodoCarta){
+            let colorAleatorioIndice= colorAleatorioIndex();
+            asignarColor(colores[colorAleatorioIndice],nodoCarta);
+            cartasAsignadas.push(nodoCarta);
+            colores.splice(colorAleatorioIndice,1);
+    })
 };
+
+function colorAleatorioIndex(){
+    return Math.floor(Math.random()*colores.length);
+};
+
+function asignarColor(color, carta){
+    carta.classList.add(color);
+    carta.classList.add("oculto");
+};
+
+function manejarRonda(){
+    for(i=0;i<cartasAsignadas.length;i++){
+        cartasAsignadas[i].addEventListener("click",evento=>manejarInput(evento));
+    }
+};
+
+function manejarInput(evento){
+    disableInput();
+    let carta= evento.target;
+    cartasClickeadas.push(carta);
+    carta.classList.remove("oculto")
+    if (cartasClickeadas.length===2){
+        compararCartas();
+    };
+    enableInput();
+}
+
+function compararCartas(){
+    disableInput();
+    setTimeout(function(){
+    if(cartasClickeadas[0].isEqualNode(cartasClickeadas[1]) && cartasClickeadas[0]!==cartasClickeadas[1]){
+        cartasClickeadas.forEach(carta=>eliminar(carta));
+        liberarCartasClickeadas()
+    } else {
+        cartasClickeadas.forEach(carta=>carta.classList.add("oculto"));
+        liberarCartasClickeadas();
+    }},1000);
+}
+function eliminar(carta){
+    carta.classList.add("eliminado");
+}
+function liberarCartasClickeadas(){
+    cartasClickeadas.pop();
+    cartasClickeadas.pop();
+}
+function disableInput() {
+    cartasDisponibles.forEach(carta=> carta.style.pointerEvents="none");
+  };
+function enableInput(){
+    cartasDisponibles.forEach(carta=> carta.style.pointerEvents="auto");
+}
