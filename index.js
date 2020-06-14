a60de19f9070774f639f32dbec2d80395953ed9d
Hernan <hsolis@est.frba.utn.edu.ar>
2020-06-14 03:59:20 -0300

    funciona completo y bloquea más de 2 inputs

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 // document.body.style.backgroundColor = "red";
 // RECORDATORIO:
-// Posiciones de cartas random en cada juego
+// Posiciones de cartas random en cada juego DONE
 // tener un timer
 // mostrar intentos
 let colores=["red","blue","darkgreen","cyan","darkslateblue","indigo","saddlebrown","aliceblue","red","blue","darkgreen","cyan","darkslateblue","indigo","saddlebrown","aliceblue"];
 let cartasDisponibles= document.querySelectorAll(".card");
-let cartasAsignadas=[];
 let cartasClickeadas=[];
 let intentos=0;
+let tiempo;
 let botonInicio= document.getElementById("botonJugar");
 botonInicio.onclick= iniciarJuego;
 
 function iniciarJuego(){
+    clearInterval(tiempo);
+    desabilitarBotonInicio();
     let cartasDisponibles= document.querySelectorAll(".card");
     asignarPosicionesAleatoriamente();
     manejarRonda();
+    iniciarTimer();
 };
 
 function asignarPosicionesAleatoriamente(){
     cartasDisponibles.forEach(function(nodoCarta){
             let colorAleatorioIndice= colorAleatorioIndex();
             asignarColor(colores[colorAleatorioIndice],nodoCarta);
-            cartasAsignadas.push(nodoCarta);
             colores.splice(colorAleatorioIndice,1);
     })
 };
@@ -36,20 +38,27 @@ function asignarColor(color, carta){
 };
 
 function manejarRonda(){
-    for(i=0;i<cartasAsignadas.length;i++){
-        cartasAsignadas[i].addEventListener("click",evento=>manejarInput(evento));
+    for(i=0;i<cartasDisponibles.length;i++){
+        
+        cartasDisponibles[i].addEventListener("click",evento=>manejarInput(evento));
     }
+    // if(estanEliminados()){
+    //     terminarRonda();
+    //     return;
+    // }
 };
 
 function manejarInput(evento){
-    disableInput();
     let carta= evento.target;
+    if(cartasClickeadas.length<1){
     cartasClickeadas.push(carta);
     carta.classList.remove("oculto")
-    if (cartasClickeadas.length===2){
+    } else if (cartasClickeadas.length<2){
+        cartasClickeadas.push(carta);
+        carta.classList.remove("oculto")
         compararCartas();
     };
-    enableInput();
+    
 }
 
 function compararCartas(){
@@ -62,6 +71,8 @@ function compararCartas(){
         cartasClickeadas.forEach(carta=>carta.classList.add("oculto"));
         liberarCartasClickeadas();
     }},1000);
+    aumentarIntentos();
+    enableInput();
 }
 function eliminar(carta){
     carta.classList.add("eliminado");
@@ -75,4 +86,48 @@ function disableInput() {
   };
 function enableInput(){
     cartasDisponibles.forEach(carta=> carta.style.pointerEvents="auto");
+}
+function iniciarTimer(){
+    let segundos =0;
+    tiempo =setInterval(() => {
+        document.getElementById("tiempo").innerHTML = `Tiempo: ${segundos} seg`;
+        segundos++
+        
+    }, 1000);
+}
+function aumentarIntentos(){
+    intentos++;
+    document.getElementById("intentos").innerHTML = `Intentos: ${intentos}`;
+}
+
+function terminarRonda(){
+    clearInterval(tiempo);
+    intentos =0;
+    habilitarBotonInicio();
+    restaurarClasesOriginalesCartas();
+    botonInicio.onclick= iniciarJuego;
+}
+function estanEliminados(){
+    let valorInicial= true;
+    for(i=0; i<cartasDisponibles.length;i++){
+         if(!cartasDisponibles[i].classList.contains("eliminado")){
+             valorInicial=false;
+         }
+    }
+    return valorInicial; 
+}
+
+function desabilitarBotonInicio(){
+    botonInicio.style.pointerEvents="none";
+};
+function habilitarBotonInicio(){
+    botonInicio.style.pointerEvents="auto";
+};
+function restaurarClasesOriginalesCartas(){
+    cartasDisponibles.forEach(carta=> carta.className = "col card");
+}
+function pasarRonda(){
+    aumentarIntentos();
+    enableInput();
+    manejarRonda();
 }
